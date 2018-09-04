f2499e074fe71430fe9929114bdf269fe94d8dea
Michael Cooper <michaelcooper@Michaels-iMac.hsd1.md.comcast.net>
2018-09-04 02:30:21 -0400

    refine animations and spacing

--- js/main.js
+++ js/main.js
@@ -1,95 +1,99 @@
-
 //animations
 
-//store classes as variables
-var effectsLink = 'animated jello';
-var effectsCard = 'animated pulse';
-var effectsModal = 'animated flipInX'
-//allow animations to play more than once after animation end
-var effectsEnd = 'animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd';
+var effectsModal = 'animated flipInX';
+var effectsWrap = 'wow bounceInUp'
 
-$("#navlinks li").hover(function() {
-  $(this).addClass(effectsLink).one(effectsEnd, function() {
-    //remove class after animation plays once to allow it to play again
-    $(this).removeClass(effectsLink);
+$(".modal").addClass(effectsModal);
+
+$("#welcome , #inspolinks h2 , #inspolinks li" , "themename , #home").addClass('animated');
+
+
+$('#inspo , .inspo , .textblock , #themes , #ask , #ask_form , #blogcontain').addClass(effectsWrap);
+
+
+//smooth scrolling from w3schools
+
+$("a").click(function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 500, function() {
+      window.location.hash = hash;
+    });
+  }
+});
+
+//modals
+
+$('a.open-modal').click(function(event) {
+  $(this).modal({
+    fadeDuration: 150,
+    fadeDelay: 0,
+    showClose: false,
+  });
+  return false;
+});
+
+//sticky scrollTop
+
+var stickyTop = $('#navlinks').offset().top;
+
+$(window).scroll(function() {
+  var windowTop = $(window).scrollTop();
+
+  if (stickyTop < windowTop) {
+    $('#navlinks').css('position', 'fixed').addClass('shadow');
+  } else {
+    $('#navlinks').css('position', 'relative').removeClass('shadow');
+  }
+});
+
+//scroll to top & home button
+
+var duration = 250;
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 800) {
+    $('#top , #home').fadeIn(duration);
+  } else {
+    $('#top , #home').fadeOut(duration);
+  }
+});
+
+$('#top').click(function(event) {
+  event.preventDefault();
+  $('html').animate({
+    scrollTop: 0
+  }, duration);
+  return false;
+})
+
+//unnest captions
+
+$(function() {
+  $('.post').unnest({
+    yourCaption: ".caption",
+    wrapName: ".reblog",
+    newCaptionUsername: false,
+    originalPostCaptionUsername: false,
+    tumblrAvatars: true,
+    tumblrAvatarClass: ".avatar",
+    usernameColon: false
   });
 });
 
-$(".themecard").hover(function() {
-  $(this).addClass(effectsCard).one(effectsEnd, function() {
-    //remove class after animation plays once to allow it to play again
-    $(this).removeClass(effectsCard);
+
+//pxu photoset
+
+$(document).ready(function() {
+  $('.photo-slideshow').pxuPhotoset({
+    lightbox: true,
+    rounded: false,
+    gutter: '1px',
+    borderRadius: '0px',
+    photoset: '.photo-slideshow',
+    photoWrap: '.photo-data',
+    photo: '.pxu-photo'
   });
 });
-
-
-  $(".modal").addClass(effectsModal);
-
-  $("#inspolinks h2 , #inspolinks li").addClass('animated')
-
-  $('#inspolinks li').addClass("shadow");
-
-
-  //smooth scrolling
-
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 500, function() {
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-
-  //modals
-
-  $('a.open-modal').click(function(event) {
-       $(this).modal({
-         fadeDuration: 150,
-         fadeDelay: 0,
-         showClose: false,
-       });
-       return false;
-     });
-
-  //sticky scrollTop
-  var stickyTop = $('#navlinks').offset().top;
-
-  $(window).scroll(function() {
-    var windowTop = $(window).scrollTop();
-
-    if (stickyTop < windowTop) {
-      $('#navlinks').css('position', 'fixed').addClass('shadow');
-    } else {
-      $('#navlinks').css('position', 'relative').removeClass('shadow');
-    }
-  });
-
-  //scroll to top
-
-  var duration = 500;
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > 800) {
-				$('#top').fadeIn(duration);
-			} else {
-				$('#top').fadeOut(duration);
-			}
-		});
-
-		$('#top').click(function(event) {
-			event.preventDefault();
-			jQuery('html').animate({scrollTop: 0}, duration);
-			return false;
-		})
